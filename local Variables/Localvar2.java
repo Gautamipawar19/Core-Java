71e917941cc3017f3abae79ecda43e21b78afac7
Gautamipawar19 <gautamipawar19@gmail.com>
2025-04-18 22:35:34 +0530

    local Variables

---
+++ local Variables/Localvar2.java
@@ -0,0 +1,11 @@
+class Localvar2
+{
+	public static void main(String args[])
+	{
+		int a;
+		//Directly
+		System.out.println(a);
+	}
+}
+ 
+ //variable a might not have been initialized
---
+++ local Variables/localvar1.java
@@ -0,0 +1,9 @@
+class Localvar1
+{
+	public static void main(String args[])
+	{
+		int a=100;
+		//Directly
+		System.out.println(a);
+	}
+}
