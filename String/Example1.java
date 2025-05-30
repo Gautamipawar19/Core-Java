a36302af93f6a72c4a2e3e9b659eb1a91ac93ead
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-30 23:24:27 +0530

    String Class
    There 2 ways to display String base object.

---
+++ String/Example1.java
@@ -0,0 +1,12 @@
+class Example1
+{		   
+	public static void main(String args[])
+	{
+		//1 Approach -> with using new keyword
+		String Str1 = new String("Cyber");
+		System.out.println(Str1);
+		//2 Approach -> without using new keyword
+		String Str2 = "Success";
+		System.out.println(Str2);
+	}
+}
