63c7b4356f577172f650d135c6ee507cc66d5980
Gautamipawar19 <gautamipawar19@gmail.com>
2025-05-30 23:33:00 +0530

    String
    With new keyword

---
+++ String/Example3.java
@@ -0,0 +1,12 @@
+class Example3
+{
+	public static void main(String args[])
+	{
+		String Str1 = new String("Cyber");
+		String Str2 = new String("Success");
+		String Str3 = new String("Cyber");
+		System.out.println(Str1);
+		System.out.println(Str2);
+		System.out.println(Str3);
+	}
+}
