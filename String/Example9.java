6f0b949477084a61851141c11d3d8582c982b8f9
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-01 11:17:03 +0530

    StringBuffer
    It is not possible create object without new keyword in StringBuffer.

---
+++ String/Example9.java
@@ -0,0 +1,10 @@
+class Example9	 
+{
+	public static void main(String args[])
+	{		   
+		//StringBuffer sb1 = "Cyber";
+		StringBuffer sb1 = new StringBuffer("Cyber");
+		System.out.println(sb1);
+		System.out.println(sb1.toString());
+	}
+}
