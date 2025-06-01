c5b6e103a1a527d2e9f55f627d01249728a4ba68
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-01 11:20:52 +0530

    StringBuffer
    StringBuffer by nature it is mutable class, which means object data can change.

---
+++ String/Example10.java
@@ -0,0 +1,11 @@
+class Example10
+{
+	public static void main(String args[])
+	{
+		StringBuffer sb1 = new StringBuffer("Cyber");
+		System.out.println(sb1);
+		sb1.append("Technology");
+		System.out.println(sb1);
+	}
+}
+//public StringBuffer append(String);
