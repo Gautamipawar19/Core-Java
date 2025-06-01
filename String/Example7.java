09599baacd999160c21f82501933cba1b71337d6
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-01 11:00:41 +0530

    String
    String class is a Immutable class.

---
+++ String/Example7.java
@@ -0,0 +1,12 @@
+class Example7
+{
+	public static void main(String args[])
+	{
+		String str1 = new String("Cyber");
+		System.out.println(str1);
+		//System.out.println(str1.concat("Success"));
+		str1 = str1.concat("Success");
+		System.out.println(str1);
+	}
+}
+//  public String concat(java.lang.String);
