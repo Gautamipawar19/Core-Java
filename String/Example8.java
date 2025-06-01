a0af04b50e105c2338c27ff47d3d5030900904dd
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-01 11:05:08 +0530

    String
    String using a length() method.

---
+++ String/Example8.java
@@ -0,0 +1,16 @@
+class Example8
+{
+	public static void main(String args[])
+	{
+		String username = "Cyber";
+		System.out.println(username.length());
+		if(username.length()>=7)
+		{
+			System.out.println("valid Username");
+		}
+		else
+		{
+			System.out.println("Invalid Username");
+		}
+	}
+}											   
