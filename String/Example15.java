59f62053b8a48c8392202752cfdfb0be92e9b02a
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-02 22:19:27 +0530

    String
    It uses a different methods in String.

---
+++ String/Example15.java
@@ -0,0 +1,15 @@
+class Example15
+{
+	public static void main(String args[])
+	{
+		String str = "CyberSuccess";
+		System.out.println(str.charAt(1));
+		//System.out.println(str.charAt(12));
+		System.out.println(str.indexOf("e"));
+		System.out.println(str.indexOf("z"));
+		System.out.println(str.lastIndexOf("r"));
+		System.out.println(str.lastIndexOf("x"));
+		System.out.println(str.contains("cce"));
+		System.out.println(str.contains("axy"));
+	}
+}
---
+++ String/Example16.java
@@ -0,0 +1,14 @@
+class Example16
+{
+	public static void main(String args[])
+	{
+		String str = "CyberSuccess";
+		System.out.println(str.startsWith("Cyb"));
+		System.out.println(str.startsWith("abc"));
+		System.out.println(str.endsWith("ess"));
+		System.out.println(str.endsWith("cyb"));
+		System.out.println(str.isEmpty());
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+	}
+}
