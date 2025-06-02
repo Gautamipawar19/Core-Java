164749a368bae83f0ac35e97b02263352ccad0cd
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-02 22:20:19 +0530

    String
    It uses a different methods in String.

---
+++ String/Example17.java
@@ -0,0 +1,10 @@
+class Example17
+{
+	public static void main(String args[])
+	{
+		String str = "CyberSuccessTraining";
+		System.out.println(str.substring(5));
+		//System.out.println(str.substring(50));
+		System.out.println(str.substring(5,12));
+	}
+}
---
+++ String/Example18.java
@@ -0,0 +1,11 @@
+class Example18
+{
+	public static void main(String args[])
+	{
+		String str = "Resume.xlsx";
+		//int dot = str.indexOf(" ");
+		//String ss = str.subString(dot +1);
+		//System.ou.println(ss);
+		System.out.println(str.substring(str.indexOf(".")+1));
+	}
+}
