03c6acac6fc3525c5602ac21380d52fe4a71336b
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-03 23:45:37 +0530

    StringBuilder
    StringBuilder is a mutable class.

---
+++ String/Example25.java
@@ -0,0 +1,10 @@
+class Example25				 
+{
+	public static void main(String args[])
+	{
+		//StringBuilder sb1 ="Cyber";
+		StringBuilder sb1 = new StringBuilder("technology");
+		System.out.println(sb1);
+		System.out.println(sb1.reverse());
+	}
+}
---
+++ String/Example26.java
@@ -0,0 +1,9 @@
+class Example26
+{
+	public static void main(String args[])
+	{
+		StringBuilder sb1 = new StringBuilder("Cyber");
+		System.out.println(sb1);
+		System.out.println(sb1.reverse());
+	}
+} 
