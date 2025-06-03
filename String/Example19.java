815e4a4f2f098ba02baedf26f27e61470d8a4339
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-03 23:41:13 +0530

    String
    Trim method can not remove the space in string.

---
+++ String/Example19.java
@@ -0,0 +1,14 @@
+class Example19
+{
+	public static void main(String args[])
+	{
+		String str1 = " Cyber ";
+		System.out.println(str1.length());
+		String str2 = str1.trim();
+		System.out.println(str2.length());
+		String ss1 = "Power on";
+		System.out.println(ss1.length());
+		String ss2 = ss1.trim();
+		System.out.println(ss2.length());
+	}
+}
