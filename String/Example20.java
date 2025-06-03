3803960724d0bd92370b3267c50f8214e4d3edf6
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-03 23:42:01 +0530

    String
    String[] method

---
+++ String/Example20.java
@@ -0,0 +1,17 @@
+class Example20
+{
+	public static void main(String args[])
+	{
+		int[] a = new int[3];
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		/*System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);*/
+		for(int i=0 ;i<3; i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}																																    
---
+++ String/Example21.java
@@ -0,0 +1,14 @@
+class Example21
+{	 public static void main(String args[])
+	{
+		int[] a= new int[3];
+		a[0] =10;
+		a[1] =20;
+		a[2] =30;
+		System.out.println(a.length);
+		for(int i=0; i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
