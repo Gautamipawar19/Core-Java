85f6d8176bf149afdc880f2a8816c39e3238d7ac
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-04 23:27:19 +0530

    Checked Exception
    It check not by the JVM  checkd by compiler.

---
+++ Exception/Example2.java
@@ -0,0 +1,12 @@
+class Example2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Start @ Cyber Success");
+		int[] a ={10, 20, 30};
+		System.out.println(a[10]);
+		System.out.println("Stop @ Cyber Success");
+	}
+}
+//java.lang.ArrayIndexOutOfBoundsException
+//It is an Abnormal Termination
