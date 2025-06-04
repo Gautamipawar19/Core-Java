bec1eae963ca06f5c8edaf7cfe6907c46d19df00
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-04 23:31:29 +0530

    Checked Exception
    It check by the compiler.

---
+++ Exception/Example3.java
@@ -0,0 +1,10 @@
+class Example3
+{
+	public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		Thread.sleep(3000);// milli Seconds
+		System.out.println("Stop @ Cyber Success");
+	}
+}
+
