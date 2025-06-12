729df23b641ee0ec63bfe90adcff9cd904f04288
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-12 21:16:12 +0530

    Exception Handling
    It is mandatory we have to use try with catch we can also ues

---
+++ Exception/Example23.java
@@ -0,0 +1,16 @@
+class Example23
+{	 
+	public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		try
+		{
+			System.out.println("Inside Try Block");
+		}
+		finally
+		{
+			System.out.println("Inside Finally Block");
+		}
+		System.out.println("Stop @ Cyber Success");
+	}
+}
