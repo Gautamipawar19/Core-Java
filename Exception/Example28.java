07a75c92c82274278430c9acbde4136746854d29
Gautamipawar19 <gautamipawar19@gmail.com>
2025-06-12 21:24:46 +0530

    Exception Handling
    Independent finally block are not allowed

---
+++ Exception/Example28.java
@@ -0,0 +1,13 @@
+class Example28
+{
+    public static void main(String args[])
+	{
+		System.out.println("Start @ Cyber Success");
+		System.out.println(10/0);
+		finally
+		{
+			System.out.println("Inside Finally Block");
+		}
+		System.out.println("Stop @ Cyber Success");
+	}
+}
