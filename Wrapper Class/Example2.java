f12aca8b796c7a0f4233cd7cfb97940c023d2352
Gautamipawar19 <gautamipawar19@gmail.com>
2025-07-08 22:41:09 +0530

    Wrapper Class is by nature immutable classes.

---
+++ Wrapper Class/Example2.java
@@ -0,0 +1,10 @@
+class Example2
+{
+	public static void main(String args[])
+	{
+		Integer i1 =new Integer("abc");
+		System.out.println(i1);
+	}
+}
+//throws -> Docu to inform it wll the=row an exception
+//oublic java.lang.Integer(String) throws NumberFormatException;
---
+++ Wrapper Class/Example3.java
@@ -0,0 +1,11 @@
+class Example3
+{
+	public static void main(String args[])
+	{
+		Character c =new Character('x'); 
+		System.out.println(c);
+		System.out.println(c.toString());
+	}
+}			  
+
+//public Character(char);
