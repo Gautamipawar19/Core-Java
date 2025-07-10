e5105a304c10bc03188aee6f58150b21aecf9d3f
Gautamipawar19 <gautamipawar19@gmail.com>
2025-07-10 23:29:03 +0530

    Wrapper Class

---
+++ Wrapper Class/Example4.java
@@ -0,0 +1,9 @@
+class Example4
+{
+	public static void main(String args[])
+	{
+		Character c = new Character('x');
+		System.out.println(c);
+		System.out.println(c.toString());
+	}
+}
---
+++ Wrapper Class/Example5.java
@@ -0,0 +1,12 @@
+class Example5
+{
+	public static void main(String args[])
+	{
+		Float f1 = new Float(10.5f);
+		Float f2 = new Float (20.5);
+		Float f3 = new Float("100");
+		System.out.println(f1);
+		System.out.println(f2);
+        System.out.println(f3);
+	}
+}
