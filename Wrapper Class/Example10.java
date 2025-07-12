3c4dc1224f0f6ec811611acbaa2016aac8a4b99e
Gautamipawar19 <gautamipawar19@gmail.com>
2025-07-12 22:26:17 +0530

    Wrapper Class

---
+++ Wrapper Class/Example10.java
@@ -0,0 +1,10 @@
+class Example10
+{	 
+	public static void main(String args[])
+	{
+		//UnBoxing 
+		int jj = new Integer(250);
+		System.out.println(jj);
+		//System.out.println(jj.toString());
+	}
+}
